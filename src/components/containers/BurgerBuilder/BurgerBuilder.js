f017ece07e879e804c4b9514c46b9b03566aef5c
Farshad <farshadhassanpoor@gmail.com>
2020-03-08 21:33:47 +0330

    burger component empty ingredients handling

--- src/components/containers/BurgerBuilder/BurgerBuilder.js
+++ src/components/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,10 +5,10 @@ import Burger from "../../Burger/Burger";
 class BurgerBuilder extends React.Component {
   state = {
     ingredients: {
-      salad: 3,
-      bacon: 2,
-      cheese: 5,
-      meat: 9
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     }
   };
 
