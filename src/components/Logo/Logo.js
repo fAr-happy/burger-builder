975cd4ee3e75d80f5d812bbec3e804e95106623b
Farshad <farshadhassanpoor@gmail.com>
2020-03-12 23:04:04 +0330

    create logo and adding to toolbar

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import burgerLogo from "../../assets/burger-logo.png";
+import styles from "./Logo.module.css";
+
+const Logo = () => {
+  return (
+    <div className={styles.Logo}>
+      <img  src={burgerLogo} alt="MyBurger"></img>
+    </div>
+  );
+};
+
+export default Logo;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import styles from './Toolbar.module.css'
+import React from "react";
+import styles from "./Toolbar.module.css";
+import Logo from "../../Logo/Logo";
 
 const Toolbar = () => {
-    return(
-        <header className={styles.Toolbar}>
-            <div>LOGO</div>
-            <div>MENU</div>
-            <ul>...</ul>
-        </header>
-    )
-}
+  return (
+    <header className={styles.Toolbar}>
+      <div>MENU</div>
+        <Logo />
+      <ul>...</ul>
+    </header>
+  );
+};
 
 export default Toolbar;
