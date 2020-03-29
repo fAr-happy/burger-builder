85702809f3d33d29ca1f96310ae52db1dff045bc
Farshad <farshadhassanpoor@gmail.com>
2020-03-29 06:04:37 +0330

    redux devtool advanced

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
-import { createStore, compose } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 import reducer from "./store/reducer";
 
-/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  /* preloadedState, */ composeEnhancers(applyMiddleware(thunk))
 );
-/* eslint-enable */
 
 const app = (
   <Provider store={store}>
