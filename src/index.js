f76841a5134f3a7ba55d60c47c8664655b72c583
Farshad <farshadhassanpoor@gmail.com>
2020-03-29 05:52:34 +0330

    redux devtools

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./store/reducer";
 
-const store = createStore(reducer);
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+/* eslint-enable */
 
 const app = (
   <Provider store={store}>
